a7764ccc271dbb4316c98ea021be57fc53d4925c
zyclamenace <gemicieneshakki@gmail.com>
2024-08-19 11:20:39 +0200

    fix 40 lines for Betty

--- simple_shell.c
+++ simple_shell.c
@@ -15,67 +15,44 @@
  */
 int main(void)
 {
-    char *input_user = NULL;
-    size_t input_length = 0;
-    ssize_t command_size;
-    pid_t child_pid;
-    int child_status;
-    char *args[2];
+	char *input_user = NULL, *args[2];
+	size_t input_length = 0;
+	ssize_t command_size;
+	pid_t child_pid;
+	int child_status;
 
-    while (1)
-    {
-        /* Afficher le prompt */
-        printf("$ ");
-        command_size = getline(&input_user, &input_length, stdin);
-        if (command_size == -1)
-        {
-            if (errno == 0)
-            {
-                /* EOF atteint, quitter la boucle */
-                printf("\n");
-                break;
-            }
-            else
-            {
-                perror("Erreur de lecture de la ligne");
-                continue;
-            }
-        }
-
-        /* Supprimer le caractère de nouvelle ligne */
-        if (input_user[command_size - 1] == '\n')
-            input_user[command_size - 1] = '\0';
-
-        /* Vérifier si l'utilisateur veut quitter */
-        if (strcmp(input_user, "exit") == 0)
-            break;
-
-        /* Créer un processus enfant */
-        child_pid = fork();
-        if (child_pid == -1)
-        {
-            perror("Erreur de fork");
-            continue;
-        }
-
-        if (child_pid == 0)
-        {
-            /* Dans le processus enfant */
-            args[0] = input_user; /* Commande à exécuter */
-            args[1] = NULL; /* Terminaison du tableau d'arguments */
-            if (execve(args[0], args, NULL) == -1)
-            {
-                perror("Erreur d'exécution de la commande");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Dans le processus parent */
-            wait(&child_status); /* Attendre que l'enfant se termine */
-        }
-    }
-
-    free(input_user);
-    return (0);
+	while (1)
+	{	printf("$ ");
+		command_size = getline(&input_user, &input_length, stdin);
+		if (command_size == -1)
+		{
+			perror("Erreur de lecture de la ligne");
+			continue;
+		}
+		if (input_user[command_size - 1] == '\n')
+			input_user[command_size - 1] = '\0';
+		if (strcmp(input_user, "exit") == 0)
+			break;
+		child_pid = fork();
+		if (child_pid == -1)
+		{	perror("Erreur de fork");
+			continue;
+		}
+		if (child_pid == 0)
+		{
+			args[0] = input_user; /* Commande à exécuter */
+			args[1] = NULL; /* Terminaison du tableau d'arguments */
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("Erreur d'exécution de la commande");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&child_status); /* Attendre que l'enfant se termine */
+		}
+	}
+	free(input_user);
+	return (0);
 }
