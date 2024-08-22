0bec4ffcc4dee44af27af95e644c886afc7eb94f
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-22 11:16:30 +0200

    Fix : Separate execute command in its own file
    - Deleted unused variable "command_size"
    - Added "()" after "read_line" to make it work
    - Remove the check for command to be executable (moved to execute command)
    - Added "built-in" command
    - Change the comment from french to english

--- main.c
+++ main.c
@@ -1,68 +1,31 @@
 #include "simpleshell.h"
-/**
- * execute_command - Crée un processus enfant pour exécuter une commande
- * @command: Commande à exécuter
- * @argv: Nom du programme pour les messages d'erreur
- * @env: Variables d'environnement
- */
-void execute_command(char *command, char **argv, char **env)
-{
-	pid_t child_pid;
-	int child_status;
-	char *args[2];
 
-	child_pid = fork(); /*Créer un processus enfant*/
-	if (child_pid == -1)
-	{
-		perror("Erreur de fork");
-		return;
-	}
-
-	if (child_pid == 0)
-	{
-		args[0] = command; /*Commande à exécuter*/
-		args[1] = NULL;	   /*Terminaison du tableau d'arguments*/
-		if (execve(args[0], args, env) == -1)
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], command);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(&child_status); /*Attendre que l'enfant se termine*/
-	}
-}
 /**
- * main - Un shell simple qui exécute des commandes avec leur chemin complet
+ * main - A simple shell which execute commands with their complete path
  * @argc: Argument count
  * @argv: Argument array
- * @env: Environment variables pour le main
+ * @env: Var environement for main
  *
- * Return: Toujours 0.
+ * Return: Always 0.
  */
+
 int main(int argc, char **argv, char **env)
 {
 	char *command = NULL;
 	char **tokenised_command = NULL;
-	size_t command_size = 0;
 
-	(void)argc; /*UNUSED*/
+	(void)argc; /*Unused var*/
 	while (1)
 	{
-		if (isatty(STDIN_FILENO)) /* DISPLAY PROMPT */
+		if (isatty(STDIN_FILENO)) /* Display prompt */
 			printf("$ ");
 
-		command = read_line;
+		command = read_line();
 
-		tokenised_command = split_string_in_token(command);/*TOKENISE LA COMMAND*/
-		if (access(tokenised_command, X_OK) == -1) /*Vérif command est exécutable*/
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], tokenised_command);
-			continue;
-		}
+		tokenised_command = split_string_in_token(command);/** Tokenised "command" */
 
-		execute_command(tokenised_command, argv, env); /*fork pour exéc commande*/
+		built_in(command, env);
+		execute_command(tokenised_command, argv, env); /*fork to execute command*/
 	}
 	free(command);
 	return (0);
