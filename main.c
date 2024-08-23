eb7311bb1a4012702d22a6583884dce4618c9e28
ArcturusSky <54417571+ArcturusSky@users.noreply.github.com>
2024-08-23 10:47:16 +0200

    Redaction: Adjust indentation in main function
    - Aligned the opening brace of the main function with the function signature formatting and indentation
    - Expanded description of built-in command handling

--- main.c
+++ main.c
@@ -9,44 +9,44 @@
  * Return: Always 0.
  */
 
-	int main(int argc, char **argv, char **env)
+int main(int argc, char **argv, char **env)
+{
+	char *command = NULL;											/** Store user input command */
+	char **tokenised_command = NULL;								/** Store tokenized command */
+	int value_returned_built_in;
+
+	(void)argc;														/** Suppress unused variable warning */
+	while (1)														/** Infinite loop for shell prompt */
 	{
-		char *command = NULL;											/** Store user input command */
-		char **tokenised_command = NULL;								/** Store tokenized command */
-		int value_returned_built_in;
+		if (isatty(STDIN_FILENO))									/** Check if input is from terminal */
+			printf("$ ");											/** Display shell prompt */
 
-		(void)argc;														/** Suppress unused variable warning */
-		while (1)														/** Infinite loop for shell prompt */
+		command = read_line();										/** Read user input and stores in command */
+
+		tokenised_command = split_string_in_token(command);			/** Token command */
+		if (tokenised_command == NULL)								/** Check if tokenization failed */
 		{
-			if (isatty(STDIN_FILENO))									/** Check if input is from terminal */
-				printf("$ ");											/** Display shell prompt */
-
-			command = read_line();										/** Read user input and stores in command */
-
-			tokenised_command = split_string_in_token(command);			/** Token command */
-			if (tokenised_command == NULL)								/** Check if tokenization failed */
-			{
-				free(command);											/** Free memory allocated for command */
-				continue;												/** Skip to next iteration */
-			}
-			value_returned_built_in = built_in(tokenised_command, env);
-			if (value_returned_built_in == 2)							/** If returned "2": exit command */
-			{
-				free(command);											/** Free memory allocated for command */
-				free_tokenised_command(tokenised_command);				/** Free tokenized command */
-				exit(EXIT_SUCCESS);										/** Exit shell */
-			}
-			else if (value_returned_built_in == 0)						/** If other built-in command */
-			{
-				free(command);											/** Free memory allocated for command */
-				free_tokenised_command(tokenised_command);				/** Free tokenized command */
-				continue;												/** Skip to next iteration */
-			}
-
-			execute_command(tokenised_command, env, argv[0]);			/** Exec given comm */
-
-			free(command);												/** Free memory allocated for command */
-			free_tokenised_command(tokenised_command);					/** Free tokenized command */
+			free(command);											/** Free memory allocated for command */
+			continue;												/** Skip to next iteration */
 		}
-		return (0);
+		value_returned_built_in = built_in(tokenised_command, env);
+		if (value_returned_built_in == 2)							/** If returned "2": exit command */
+		{
+			free(command);											/** Free memory allocated for command */
+			free_tokenised_command(tokenised_command);				/** Free tokenized command */
+			exit(EXIT_SUCCESS);										/** Exit shell */
+		}
+		else if (value_returned_built_in == 0)						/** If other built-in command */
+		{
+			free(command);											/** Free memory allocated for command */
+			free_tokenised_command(tokenised_command);				/** Free tokenized command */
+			continue;												/** Skip to next iteration */
+		}
+
+		execute_command(tokenised_command, env, argv[0]);			/** Exec given comm */
+
+		free(command);												/** Free memory allocated for command */
+		free_tokenised_command(tokenised_command);					/** Free tokenized command */
 	}
+	return (0);
+}
